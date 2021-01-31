931d2396fe7199502b8d752403ebd04cac0f5cbe
PritzkerJ <jacobpritzker@gmail.com>
2021-01-31 09:49:43 +0000

    comments

--- tiling/main_tiling.py
+++ tiling/main_tiling.py
@@ -18,23 +18,29 @@ for r in range(num_rows):
 grid = set(grid_points)
 grid = {(0,0),(1,0),(2,0),(0,1),(0,2),(1,2),(2,2),(2,1)}
 
+#lagrangean
 gamma = 10*len(grid) + 1
 
+#number of orientations of a 2D tile
 num_orientations = 8
 
+#define tile
 tile = [(0,0),(1,0),(0,1)]
 num_squares_in_tile = len(tile)
-tiles = get_orientations(tile) #... all the orientation
 
+#generate all orientations of tile
+tiles = get_orientations(tile)
+
+#determine invalid tile placements and orientations
 location_log = {}
 out_of_bounds_log = {}
 
 for prime_location in grid:
     for orientation in range(1,num_orientations+1):
         for tile_square_index in range(num_squares_in_tile):
-            offset = tiles[orientation][tile_square_index] #orientation may be off by one
+            offset = tiles[orientation][tile_square_index]
 
-            #calculate location of new tile
+            #calculate locations covered by tile
             location = tuple([i+j for i,j in zip(prime_location, offset)])
 
             #check if off grid
@@ -43,7 +49,6 @@ for prime_location in grid:
                     out_of_bounds_log[prime_location].append(orientation)
                 else:
                     out_of_bounds_log[prime_location] = [orientation]
-                #no need to log these - just set coefficient here
             else:
                 #log that this tile would hit this location
                 if location in location_log:
@@ -51,7 +56,10 @@ for prime_location in grid:
                 else:
                     location_log[location] = [(prime_location, orientation)]
 
-#Setting linear costs
+#setting linear costs:
+#-1 for a valid tile placement
+#gamma for an invalid tile placement
+#0 otherwise
 for prime_location in grid:
     dqm.add_variable(num_orientations+1, label=prime_location)
 for prime_location in grid:
@@ -61,8 +69,8 @@ for prime_location in grid:
             costs[orientation] = gamma
     dqm.set_linear(prime_location, costs)
 
+#determine invalid pairs of tile placements
 overlap_violations = {}
-
 for location in grid:
     log = sorted(location_log[location])
     length = len(log)
@@ -76,9 +84,13 @@ for location in grid:
                 else:
                     overlap_violations[(prime_location0, prime_location1)] = [(orientation0, orientation1)]
 
+#setting quadratic costs:
+#gamma for an overlapping pair of placements
+#0 otherwise
 for prime_location0, prime_location1 in overlap_violations:
     dqm.set_quadratic(prime_location0, prime_location1, {elem: gamma for elem in overlap_violations[(prime_location0, prime_location1)]})
 
+#solve problem
 print("sending to leap")
 start_time = time.time()
 sampler = LeapHybridDQMSampler()
@@ -88,9 +100,10 @@ energy = sampleset.first.energy
 end_time = time.time()
 print("took", end_time-start_time, "seconds")
 
+#display optimal tiling
 disp = TileDisplay(grid=grid)
 for location in sample:
     orientation = sample[location]
     disp.add_tile(location, tiles[orientation])
 print(disp)
-print(str(round(-energy/10)) + " fit in the grid!")
+print(str(round(-energy/10)) + " tiles fit in the grid!")
