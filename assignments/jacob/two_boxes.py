e4c2e107c275d6c9f49345017e67670cc9c0d53a
PritzkerJ <jacobpritzker@gmail.com>
2021-01-31 01:45:59 +0000

    Hybrid solver for two boxes example

--- assignments/jacob/two_boxes.py
+++ assignments/jacob/two_boxes.py
@@ -1,12 +1,21 @@
 import dimod
+from dwave.system import LeapHybridSampler
 
 exactsolver = dimod.ExactSolver()
 
 Q = {(0, 0): -58, (1, 1): -54, (2,2): -56, (0,1): 50, (0,2): 50, (1,2): 50}
 
 bqm = dimod.BinaryQuadraticModel.from_qubo(Q, offset=100)
-results = exactsolver.sample(bqm)
 
+'''
+results = exactsolver.sample(bqm)
 # print the results
 for sample, energy in results.data(['sample', 'energy']):
     print(sample, energy)
+'''
+
+sampler = LeapHybridSampler()
+response = sampler.sample(bqm, time_limit=5)
+
+print("Optimal value:", response.first.energy)
+print("Optimal solution:", response.first.sample)
