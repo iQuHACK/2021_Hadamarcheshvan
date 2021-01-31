4955726a801bd7a4b001ec1c7658db48e35d652a
JosephFeld <josephfeld2019@gmail.com>
2021-01-31 01:52:41 +0000

    bad sum boxes

---
+++ assignments/joseph/sumboxes.py
@@ -0,0 +1,37 @@
+import dimod
+
+exactsolver = dimod.ExactSolver()
+
+Q = {(0, 0): -1, (1, 1): -1, (2, 2): -1, (0,2): 2, (0,1): 2, (1,2): 2}
+
+
+
+Q_objective = {(0,0): 17, (1,1): 21, (2,2): 19}
+Q_constraint = {}
+for i in range(0,3):
+    Q_constraint[(i,i)] = -1 #squared terms and linear terms add to be linear
+    for j in range(i+1,3):
+        Q_constraint[(i,j)] = 2 #cross terms
+
+lagrange = 25
+c = 4
+
+Q = dict(Q_objective)
+
+#combine QUBOs
+for key in Q_constraint:
+    if key in Q:
+        Q[key] += lagrange * Q_constraint[key]
+    else:
+        Q[key] = lagrange * Q_constraint[key]
+
+print(Q)
+
+results = exactsolver.sample_qubo(Q)
+
+bqm = dimod.BinaryQuadraticModel.from_qubo(Q, offset=c*lagrange)
+results = exactsolver.sample(bqm)
+
+# print the results
+for sample, energy in results.data(['sample', 'energy']):
+    print(sample, energy)
