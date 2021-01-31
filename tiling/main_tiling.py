0aa9d5fff05088ef8f1434684c5c28eb3defba0a
PritzkerJ <jacobpritzker@gmail.com>
2021-01-31 13:36:34 +0000

    multiple tiles

--- tiling/main_tiling.py
+++ tiling/main_tiling.py
@@ -2,22 +2,22 @@ from dimod import DiscreteQuadraticModel
 from dwave.system import LeapHybridDQMSampler
 from tile_orientations import get_orientations
 from graphics import TileDisplay
+import numpy as np
 import time
 dqm = DiscreteQuadraticModel()
 
 #dimensions of grid
-num_rows = 12
-num_cols = 6
+num_rows = 6
+num_cols = 3
 
 #generate rectangular grid
 grid_points = []
 for r in range(num_rows):
     for c in range(num_cols):
         grid_points.append((r,c))
-        
 
 grid = set(grid_points)
-#grid = {(0,0),(1,0),(2,0),(0,1),(0,2),(1,2),(2,2),(2,1)}
+grid = {(0,0),(0,1),(0,2),(1,0),(1,1)}
 
 #lagrangean
 gamma = 10*len(grid) + 1
@@ -25,49 +25,51 @@ gamma = 10*len(grid) + 1
 #number of orientations of a 2D tile
 num_orientations = 8
 
-#define tile
-tile = [(0,0),(1,0),(0,1)]
-num_squares_in_tile = len(tile)
+#define tiles
+tiles = [[(0,0),(1,0),(0,1)], [(0,0),(1,0)], [(0,0),(1,1)]]
+num_tiles = len(tiles)
+num_squares_in_tile = [len(tile) for tile in tiles]
 
-#generate all orientations of tile
-tiles = get_orientations(tile)
+#generate all orientations of tiles
+tiles = [get_orientations(tile) for tile in tiles]
 
 #determine invalid tile placements and orientations
 location_log = {}
 out_of_bounds_log = {}
 
 for prime_location in grid:
-    for orientation in range(1,num_orientations+1):
-        for tile_square_index in range(num_squares_in_tile):
-            offset = tiles[orientation][tile_square_index]
+    for t in range(num_tiles):
+        for orientation in range(1,num_orientations+1):
+            for tile_square_index in range(num_squares_in_tile[t]):
+                offset = tiles[t][orientation][tile_square_index]
 
-            #calculate locations covered by tile
-            location = tuple([i+j for i,j in zip(prime_location, offset)])
+                #calculate locations covered by tile
+                location = tuple([i+j for i,j in zip(prime_location, offset)])
 
-            #check if off grid
-            if location not in grid:
-                if prime_location in out_of_bounds_log:
-                    out_of_bounds_log[prime_location].append(orientation)
+                #check if off grid
+                if location not in grid:
+                    if prime_location in out_of_bounds_log:
+                        out_of_bounds_log[prime_location].append((t, orientation))
+                    else:
+                        out_of_bounds_log[prime_location] = [(t, orientation)]
                 else:
-                    out_of_bounds_log[prime_location] = [orientation]
-            else:
-                #log that this tile would hit this location
-                if location in location_log:
-                    location_log[location].append((prime_location, orientation))
-                else:
-                    location_log[location] = [(prime_location, orientation)]
+                    #log that this tile would hit this location
+                    if location in location_log:
+                        location_log[location].append((prime_location, t, orientation))
+                    else:
+                        location_log[location] = [(prime_location, t, orientation)]
 
 #setting linear costs:
 #-10 for a valid tile placement
 #gamma for an invalid tile placement
 #0 otherwise
 for prime_location in grid:
-    dqm.add_variable(num_orientations+1, label=prime_location)
+    dqm.add_variable(num_tiles*num_orientations+1, label=prime_location)
 for prime_location in grid:
-    costs = [0] + [-10]*num_orientations
+    costs = [0] + [-10]*(num_tiles*num_orientations)
     if prime_location in out_of_bounds_log:
-        for orientation in out_of_bounds_log[prime_location]:
-            costs[orientation] = gamma
+        for t, orientation in out_of_bounds_log[prime_location]:
+            costs[orientation+t*num_orientations] = gamma
     dqm.set_linear(prime_location, costs)
 
 #determine invalid pairs of tile placements
@@ -76,14 +78,14 @@ for location in grid:
     log = sorted(location_log[location])
     length = len(log)
     for i in range(length):
-        prime_location0, orientation0 = log[i]
+        prime_location0, t0, orientation0 = log[i]
         for j in range(i+1, length):
-            prime_location1, orientation1 = log[j]
+            prime_location1, t1, orientation1 = log[j]
             if prime_location0 != prime_location1:
                 if (prime_location0, prime_location1) in overlap_violations:
-                    overlap_violations[(prime_location0, prime_location1)].append((orientation0, orientation1))
+                    overlap_violations[(prime_location0, prime_location1)].append((orientation0+t0*num_orientations, orientation1+t1*num_orientations))
                 else:
-                    overlap_violations[(prime_location0, prime_location1)] = [(orientation0, orientation1)]
+                    overlap_violations[(prime_location0, prime_location1)] = [(orientation0+t0*num_orientations, orientation1+t1*num_orientations)]
 
 #setting quadratic costs:
 #gamma for an overlapping pair of placements
@@ -96,7 +98,7 @@ print("sending to leap")
 start_time = time.time()
 sampler = LeapHybridDQMSampler()
 #note that the time limit may need to be increased for especially large grid sizes
-sampleset = sampler.sample_dqm(dqm, time_limit=max(len(grid),5))
+sampleset = sampler.sample_dqm(dqm, time_limit=max(2*len(grid),5))
 sample = sampleset.first.sample
 energy = sampleset.first.energy
 end_time = time.time()
@@ -105,7 +107,11 @@ print("took", end_time-start_time, "seconds")
 #display optimal tiling
 disp = TileDisplay(grid=grid)
 for location in sample:
-    orientation = sample[location]
-    disp.add_tile(location, tiles[orientation])
+    val = sample[location]
+    if val != 0:
+        t = int(np.floor((val-1)/num_orientations))
+        orientation = val - t*num_orientations
+        tile = tiles[t]
+        disp.add_tile(location, tile[orientation])
 print(disp)
 print(str(round(-energy/10)) + " tiles fit in the grid!")
