950441bd6dabe965515db09cf46d63ea2fb54832
JosephFeld <josephfeld2019@gmail.com>
2021-01-31 06:35:40 +0000

    main loop

--- tiling/tiling.py
+++ tiling/tiling.py
@@ -5,6 +5,7 @@ grid = [(0,0), (1,1)] #...
 num_orientations = 8
 num_squares_in_tile = 3
 tile = [(0,0),(1,0),(1,1)]
+tiles = [[(0,0),(1,0),(1,1)],[(0,0),(0,1),(1,1)]] #... all the orientations
 
 location_log = {}
 out_of_bounds_log = []
@@ -13,7 +14,7 @@ for prime_location in grid:
     x, y = prime_location
     for orientation in range(num_orientations):
         for tile_square_index in range(num_squares_in_tile):
-            x_offset, y_offset = tile[tile_square_index]
+            x_offset, y_offset = tiles[orientation][tile_square_index]
 
             #calculate location of new tile
             location = (x+x_offset, y+y_offset)
@@ -22,5 +23,11 @@ for prime_location in grid:
             if location not in grid:
                 out_of_bounds_log.append((location, orientation))
             else:
-                location_log[location].append((location, orientation))
+                #log that this tile would hit this location
+                if location in location_log:
+                    location_log[location].append((location, orientation))
+                else:
+                    location_log[location] = [(location, orientation)]
+
+            
 
