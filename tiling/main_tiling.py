d3c55eab49de6b1c02e68d0fb26e1e14ff18ea25
PritzkerJ <jacobpritzker@gmail.com>
2021-01-31 08:57:52 +0000

    Extra time

--- tiling/main_tiling.py
+++ tiling/main_tiling.py
@@ -4,8 +4,8 @@ from tile_orientations import get_orientations
 from graphics import TileDisplay
 dqm = DiscreteQuadraticModel()
 
-num_rows = 2
-num_cols = 3
+num_rows = 12
+num_cols = 12
 
 grid_points = []
 for r in range(num_rows):
@@ -13,11 +13,12 @@ for r in range(num_rows):
         grid_points.append((r,c))
 
 grid = set(grid_points)
+#grid = {(0,0),(1,0),(1,1),(1,2)}
 
-gamma = len(grid) + 1
+gamma = 10*len(grid) + 1
 
 num_orientations = 8
-tile = [(0,0),(1,0)]
+tile = [(0,0),(0,1),(1,1)]
 num_squares_in_tile = len(tile)
 tiles = get_orientations(tile) #... all the orientation
 
@@ -50,7 +51,7 @@ for prime_location in grid:
 for prime_location in grid:
     dqm.add_variable(num_orientations+1, label=prime_location)
 for prime_location in grid:
-    costs = [0] + [-1]*num_orientations
+    costs = [0] + [-10]*num_orientations
     if prime_location in out_of_bounds_log:
         for orientation in out_of_bounds_log[prime_location]:
             costs[orientation] = gamma
@@ -75,7 +76,7 @@ for prime_location0, prime_location1 in overlap_violations:
     dqm.set_quadratic(prime_location0, prime_location1, {elem: gamma for elem in overlap_violations[(prime_location0, prime_location1)]})
 
 sampler = LeapHybridDQMSampler()
-sampleset = sampler.sample_dqm(dqm)
+sampleset = sampler.sample_dqm(dqm, time_limit=60)
 sample = sampleset.first.sample
 energy = sampleset.first.energy
 
@@ -85,6 +86,7 @@ disp = TileDisplay(num_rows, num_cols)
 for location in sample:
     orientation = sample[location]
     disp.add_tile(location, tiles[orientation])
-    print(disp)
+    #print(disp)
+print(disp)
 
 # locations_Log {tuple:[(tuple, int)]}
