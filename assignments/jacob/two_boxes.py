5d523cc9bd959b5b8e842416cbc70caac4116de7
PritzkerJ <jacobpritzker@gmail.com>
2021-01-31 01:38:17 +0000

    offset on two boxes example

--- assignments/jacob/two_boxes.py
+++ assignments/jacob/two_boxes.py
@@ -4,7 +4,8 @@ exactsolver = dimod.ExactSolver()
 
 Q = {(0, 0): -58, (1, 1): -54, (2,2): -56, (0,1): 50, (0,2): 50, (1,2): 50}
 
-results = exactsolver.sample_qubo(Q)
+bqm = dimod.BinaryQuadraticModel.from_qubo(Q, offset=100)
+results = exactsolver.sample(bqm)
 
 # print the results
 for sample, energy in results.data(['sample', 'energy']):
